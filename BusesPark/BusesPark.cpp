c269a8db477232991e812ab40c666f2b63b778cf
Алексей <alexei.granit.elektron@gmail.com>
2017-04-27 14:42:50 +0300

    Initial commit.

---
+++ BusesPark/BusesPark.cpp
@@ -0,0 +1,15 @@
+// BusesPark.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "routesCalculator.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello!\n");
+	calculate();
+	for(;;);
+	return 0;
+}
+
---
+++ BusesPark/routesCalculator.cpp
@@ -0,0 +1,11 @@
+#include "routesCalculator.h"
+#include "stdafx.h"
+#include <list>
+
+using namespace std;
+
+void calculate()
+{
+	printf("Calculator");
+
+}
